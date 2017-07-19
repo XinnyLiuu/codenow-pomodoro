82e04657dab834b8e64cfb546bd4b318f43f04a7
Xin Liu <xhliu97@gmail.com>
2017-07-19 04:26:52 +0000

    updated and fixed typo

--- db/migrate/20170719042524_create_todos.rb
+++ db/migrate/20170719042524_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.string :description
-      t.integer :pomodoro_etimate
+      t.integer :pomodoro_estimate
       t.boolean :complete
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170719034144) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "description"
-    t.integer  "pomodoro_etimate"
+    t.integer  "pomodoro_estimate"
     t.boolean  "complete"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
