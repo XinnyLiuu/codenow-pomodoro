d21869fb113507bf5f96cb51e504eb26257ff013
Xin Liu <xhliu97@gmail.com>
2017-07-19 03:02:33 +0000

    linked repsective show views to each to do

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,19 @@ class TodoController < ApplicationController
    end 
    
    def show
-       @todo_description = "Make the Cirrculum"
-       @todo_pomodoro_estimate = 4
+      todo_id = params[:id]
+      
+      if todo_id == '1'
+         @todo_description = "Teach the Students"
+         @todo_pomodoro_estimate = 4
+         
+      elsif todo_id == '2'
+         @todo_description = "Walk the Students to Lunch"
+         @todo_pomodoro_estimate = 3
+      
+      elsif todo_id == '3'
+         @todo_description = "Make sure the Students have Fun!"
+         @todo_pomodoro_estimate = 0
+      end
    end
 end
