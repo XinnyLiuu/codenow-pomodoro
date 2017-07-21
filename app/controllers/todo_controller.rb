2061b0e9157cba1a83c14fa697240da986e69082
Xin Liu <xhliu97@gmail.com>
2017-07-21 04:25:48 +0000

    added delete functionality;

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -16,4 +16,22 @@ class TodoController < ApplicationController
       todo.save 
       redirect_to "/todo/show/#{todo.id}"
    end
+   
+   def edit
+      @todo = Todo.find_by_id(params[:id])
+   end
+   
+   def update
+      todo = Todo.find_by_id(params[:id])
+      todo.description = params['description']
+      todo.pomodoro_estimate = params['pomodoro-estimate']
+      todo.save
+      redirect_to "/todo/show/#{todo.id}"
+   end
+   
+   def destroy
+      todo = Todo.find_by_id(params[:id])
+      todo.destroy
+      redirect_to "/todo/index"
+   end
 end
