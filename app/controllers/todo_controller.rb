ac48e53846c45516b7fa7ef4bec57a1f2f5a6261
Xin Liu <xhliu97@gmail.com>
2017-07-21 03:47:05 +0000

    added new, craete

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,6 +3,17 @@ class TodoController < ApplicationController
    end 
    
    def show
-      todo_id = params[:id]
+      @todo = Todo.find_by_id(params[:id])
+   end
+   
+   def new
+   end
+   
+   def create
+      todo = Todo.new
+      todo.description = params['description']
+      todo.pomodoro_estimate = params['pomodoro_estimate']
+      todo.save 
+      redirect_to '/todo/show#{todo.id}'
    end
 end
